2cfef727af03510df8b08ef3bd7d308d5d8d34fd
Ramneek Sharma <ramneek.sharma@gmail.com>
2022-01-22 13:10:49 +1000

    Checkin Module 2

--- ssg/site.py
+++ ssg/site.py
@@ -1,16 +1,37 @@
 from pathlib import Path
+from urllib.parse import ParseResult
+import ssg.parsers
 
 class Site:
-    def __init__(self,source, dest):
+    source: Path = None
+    dest: Path = None
+    parsers: List[ssg.parsers.Parser] = None
+
+    def __init__(self, source, dest, parsers = None):
         self.source = Path(source)
         self.dest = Path(dest)
+        self.parsers = parsers or []
 
-    def create_dir(self, path):
+    def create_dir(self, path) -> None:
         directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
 
-    def build(self):
+    def build(self) -> None:
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
+
+    def load_parser(self, extension: str) -> ssg.parsers.Parser: 
+        for parser in self.parsers:
+            if parser.valid_extension(extension):
+                return parser
+    
+    def run_parser(self, path: Path) -> None:
+        parser = self.load_parser(path.suffix)
+        if parser is not None: 
+            parser.parse(path, self.source, self.dest)
+        else:
+            print("NotImplemented")
